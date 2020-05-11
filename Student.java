baa5ebbe76624b47eeb13a4d4855dc10e15712c3
manoj1227 <55950510+manoj1227@users.noreply.github.com>
2020-05-11 13:16:31 +0530

    c_n_pojo

---
+++ Student.java
@@ -0,0 +1,11 @@
+package c_n_pojo;
+public class Student{
+	public int sno;
+	public String sname;
+	public String course;
+	public double fee;
+	public String email;
+	public long mobilenum;
+	public char gender;
+public	Address a1=new Address();
+}
